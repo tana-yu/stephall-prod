90dc65f4f15f32426ee5b8c5ba6ce7fa2c2f5182
tana-yu <gamasann21@yahoo.co.jp>
2025-08-17 16:58:12 +0900

    お問い合わせページ作成（お問い合わせフォームを除く）

---
+++ page-contact.php
@@ -0,0 +1,31 @@
+<?php
+get_header();
+?>
+
+<?php get_template_part('parts/page-top-menu'); ?>
+
+<div class="headline">
+    <?php echo tagImg('/contact/headline.svg', 'Contact'); ?>
+    <span>お問い合わせ</span>
+</div>
+
+<section class="web-contact">
+    <h2>WEBでお問い合わせ</h2>
+
+
+</section>
+
+<section class="tel-contact">
+    <h2>お電話でのお問い合わせ</h2>
+
+    <p>
+        テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+    </p>
+
+    <div class="number">
+        <a href="tel:06-6371-2264">06-6371-2264</a><span> (13:00〜22:00)</span>
+    </div>
+</section>
+
+<?php
+get_footer();

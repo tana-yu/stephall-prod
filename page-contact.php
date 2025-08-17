981a6c5fd359b80253b7a2a622faf5da4f7058ce
tana-yu <gamasann21@yahoo.co.jp>
2025-08-17 20:31:42 +0900

    contact-form機能作成

--- page-contact.php
+++ page-contact.php
@@ -12,10 +12,16 @@ get_header();
 <section class="web-contact">
     <h2>WEBでお問い合わせ</h2>
 
+    <p>
+        入力フォームは全て必須項目です。お問い合わせ後、メールまたはお電話にて折り返しご連絡させていただきます。お問い合わせを送信いただいてから3日以上返答がない場合、恐れ入りますがお電話にてご連絡ください。　お電話でのお問い合わせの方は
+        <a href="#tel-contact">コチラ</a>から。
+    </p>
+
+    <?php echo do_shortcode('[contact-form-7 id="59f5b3c" title="お問い合わせフォーム"]'); ?>
 
 </section>
 
-<section class="tel-contact">
+<section class="tel-contact" id="tel-contact">
     <h2>お電話でのお問い合わせ</h2>
 
     <p>
